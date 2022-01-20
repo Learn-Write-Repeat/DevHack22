7a140227110f38fcb21bda7f30f05492c772892f
Sonu <78155991+Sonu589@users.noreply.github.com>
2022-01-20 14:16:42 +0530

    Add files via upload

---
+++ Web Development/Sonu/Website/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $('#login').click(function(){
+        $('.login-form').addClass('popup');
+    });
+
+    $('.login-form form .fa-times').click(function(){
+        $('.login-form').removeClass('popup');
+    });
+
+    $(window).on('load scroll',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        $('.login-form').removeClass('popup');
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top - 200;
+
+            if(top > offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+
+        });
+
+    });
+
+}); 
+//   all ------------------
+function initParadoxWay() {
+    "use strict";
+   
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+                
+            }
+        });
+    }
+    
+// bubbles -----------------
+    
+    
+    setInterval(function () {
+        var size = randomValue(sArray);
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity': '-=0.7'
+        }, 4000, function () {
+            $(this).remove()
+        });
+    }, 350);
+    
+}
+
+//   Init All ------------------
+$(document).ready(function () {
+    initParadoxWay();
+});
